96ee1f4e790379d9abb34ade6cb7aa8522bcbc2d
Mateo Körber Belmonte <44673490+Matewoo@users.noreply.github.com>
2025-03-08 23:36:46 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -350,7 +350,7 @@ app.post('/generate-pdf', requireLogin, (req, res) => {
     try {
         if (type === 'week' && trans === '') {
             console.log('Generating PDF for week:', val);
-            pdfPath = `python.exe .\\scripts\\writeWeeklyMenuDE.py ${val}`;
+            pdfPath = `python3 scripts/writeWeeklyMenuDE.py ${val}`;
         } else if (type === 'week' && trans !== '') {
             console.log('Generating EN PDF for week:', val);
             
@@ -359,7 +359,7 @@ app.post('/generate-pdf', requireLogin, (req, res) => {
             fs.writeFileSync(tempJsonPath, JSON.stringify(trans, null, 2), 'utf8');
             
             // Call Python script without passing the translations as command line argument
-            pdfPath = `python.exe .\\scripts\\writeWeeklyMenuEN.py ${val}`;
+            pdfPath = `python3 scripts/writeWeeklyMenuEN.py ${val}`;
         } else if (type === 'day') {
             console.log('Generating PDF for daily:', val);
             pdfPath = `python3 scripts/writeDailyMenu.py ${val}`;
